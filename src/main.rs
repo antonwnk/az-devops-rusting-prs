890bf89e0e4c5484b4884f7a9297bc5b5da32cc7
Anton Waniek <antonwnk@users.noreply.github.com>
2022-10-10 18:57:13 +0200

    More calls still just for reference

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use azure_devops_rust_api::git;
+use azure_devops_rust_api::core::*;
 use azure_devops_rust_api::Credential;
 use dotenv::dotenv;
 use log::info;
@@ -7,8 +8,8 @@ use std::env;
 use std::sync::Arc;
 
 
-async fn get_repos(organization: &str, project: &str, credential: Credential) -> Result<Vec<git::models::GitRepository>> {
-    let git_client = git::ClientBuilder::new(credential).build();
+async fn get_repos(organization: &str, project: &str, credential: &Credential) -> Result<Vec<git::models::GitRepository>> {
+    let git_client = git::ClientBuilder::new(credential.clone()).build();
     let repos = git_client.repositories_client()
         .list(organization, project)
         .into_future()
@@ -17,6 +18,7 @@ async fn get_repos(organization: &str, project: &str, credential: Credential) ->
     Ok(repos)
 }
 
+// async fn get_contributors(organization: &str, project: &str, credential: Credential) -> Result<Vec<accounts::>
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -34,15 +36,48 @@ async fn main() -> Result<()> {
         }
     };
 
-    let organization = env::var("ADO_ORGANIZATION").expect("Specify organization with $ADO_ORAGANIZATION.");
+    let organization = env::var("ADO_ORGANIZATION").expect("Specify organization with $ADO_ORGANIZATION.");
     let project = env::var("ADO_PROJECT").expect("Specify project with $ADO_PROJECT");
 
-    let repos = get_repos(&organization, &project, credential.clone()).await?;
+    let repos = get_repos(&organization, &project, &credential).await?;
     
     for repo in repos.iter() {
         info!("{}", repo.name);
     }
     info!("{} repos found.", repos.len());
+
+
+    let core_client = ClientBuilder::new(credential.clone()).build();
+
+    let project_obj = core_client.projects_client()
+        .get(&organization, &project)
+        .into_future()
+        .await?;
+
+    println!("PROJECT OBJ");
+    println!("{:#?}", project_obj);
+    println!();
+    
+    let default_team = project_obj.default_team.unwrap();
+    let team_name = default_team.name.unwrap();
+    let team_id = default_team.id.unwrap();
+    println!("Default team name:\n{}", team_name);
+    println!("Default team GUID:\n{}", team_id);
+    println!();
+
+    println!("The members now");
+    // let plm = core_client.teams_client().get(&organization, &project, &team_id).into_future().await?;
+    let members = core_client.teams_client().get_team_members_with_extended_properties(&organization, &project, &team_id).into_future().await?;
+
+    println!("{:#?}", members);
+
+    let member_names: Vec<String> = members.value.into_iter()
+        .map(|member| member.identity)
+        .flatten()
+        .map(|identity| identity.unique_name)
+        .collect();
+
+    println!("{:?}", member_names);
 
     Ok(())
 }
