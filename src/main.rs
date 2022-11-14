ade14f4061eb1b40464f6753806a70993ce3732f
Anton Waniek <antonwnk@users.noreply.github.com>
2022-11-14 19:52:34 +0100

    Added git2 and started playing with the local repo.s

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use azure_devops_rust_api::git;
 use azure_devops_rust_api::core::*;
 use azure_devops_rust_api::Credential;
 use dotenv::dotenv;
+use git2::Repository;
 use inquire::MultiSelect;
 use log::info;
 use std::env;
@@ -40,42 +41,62 @@ async fn main() -> Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    let credential = match env::var("ADO_TOKEN") {
-        Ok(token) => {
-            info!("Authenticating with PAT from $ADO_TOKEN");
-            Credential::from_pat(token)
-        }
-        Err(_) => {
-            info!("Attempting to authenticate with Azure CLI");
-            Credential::from_token_credential(Arc::new(azure_identity::AzureCliCredential {}))
-        }
-    };
+    // let credential = match env::var("ADO_TOKEN") {
+    //     Ok(token) => {
+    //         info!("Authenticating with PAT from $ADO_TOKEN");
+    //         Credential::from_pat(token)
+    //     }
+    //     Err(_) => {
+    //         info!("Attempting to authenticate with Azure CLI");
+    //         Credential::from_token_credential(Arc::new(azure_identity::AzureCliCredential {}))
+    //     }
+    // };
 
-    let organization = env::var("ADO_ORGANIZATION").expect("Specify organization with $ADO_ORGANIZATION.");
-    let project = env::var("ADO_PROJECT").expect("Specify project with $ADO_PROJECT");
+    // let organization = env::var("ADO_ORGANIZATION").expect("Specify organization with $ADO_ORGANIZATION.");
+    // let project = env::var("ADO_PROJECT").expect("Specify project with $ADO_PROJECT");
 
-    let repos = get_repos(&organization, &project, &credential).await?;
-    
-    for repo in repos.iter() {
-        info!("{}", repo.name);
+    let repo_url: String;
+
+    if let Ok(repo) = Repository::open(env::current_dir()?) {
+        let remote = repo.find_remote("origin")?;
+        repo_url = remote.url().unwrap().to_owned(); 
+        
+        // let source_branch = 
+        
+        dbg!(repo.head().unwrap().is_branch());
+        dbg!(repo.head().unwrap().name());
+        dbg!(repo.head().unwrap().shorthand());
+        println!("{}", repo.head_detached().ok().unwrap());
+
+    } else {    
+        // then just take url from the user
+        repo_url = String::from("bleh");
     }
-    info!("{} repos found.", repos.len());
+    info!("{}", repo_url);
+
+
+    // let repos = get_repos(&organization, &project, &credential).await?;
     
-    let members = get_contributors(&organization, &project, &credential).await?;
+    // for repo in repos.iter() {
+    //     info!("{}", repo.name);
+    // }
+    // info!("{} repos found.", repos.len());
+    
+    // let members = get_contributors(&organization, &project, &credential).await?;
 
-    let member_names: Vec<String> = members.value.into_iter()
-        .map(|member| member.identity)
-        .flatten()
-        .map(|identity| identity.unique_name)
-        .collect();
+    // let member_names: Vec<String> = members.value.into_iter()
+    //     .map(|member| member.identity)
+    //     .flatten()
+    //     .map(|identity| identity.unique_name)
+    //     .collect();
 
-    println!("{:?}", member_names);
+    // println!("{:?}", member_names);
 
-    let prompty_res = MultiSelect::new("Select your witnesses:", member_names)
-        .prompt()?;
+    // let prompty_res = MultiSelect::new("Select your witnesses:", member_names)
+    //     .prompt()?;
 
 
-    println!("{:?}",  prompty_res);
+    // println!("{:?}",  prompty_res);
 
     Ok(())
 }
